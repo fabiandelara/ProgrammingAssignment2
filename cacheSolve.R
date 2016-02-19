4b5c21c977938d43ea4e20ec04eabe5bfa335465
fabiandelara <fabiandelara@prodigy.net.mx>
2016-02-19 06:28:29 -0600

    Update cacheSolve.R

--- cacheSolve.R
+++ cacheSolve.R
@@ -2,13 +2,19 @@
 # This function computes the inverse of the special "matrix" returned by makeCacheMatrix 
 
 cacheSolve <- function(x, ...) {
+  
   g_inv <- x$getInverse()
+  
   if(!is.null(g_inv)) {
     message("getting cache dats")
     return(g_inv)
   }
+  
   data <- x$get()
+  
   g_inv <- solve(data, ...)
+  
   x$setInverse(g_inv)
+  
   g_inv
 }
