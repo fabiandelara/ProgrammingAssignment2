387d4946daaadd0565ac3c50fade907e26e0143b
fabiandelara <fabiandelara@prodigy.net.mx>
2016-02-19 06:27:10 -0600

    Update makeCacheMatrix.R

--- makeCacheMatrix.R
+++ makeCacheMatrix.R
@@ -3,13 +3,22 @@
 
 makeCacheMatrix <- function(x = matrix) {
   matr <- NULL
+  
+  # Setting the matrix value 
   set <- function(y) {
     x <<- y 
     matr <<- NULL 
   }
+  
+  # Getting the matrix value 
   get <- function() x
+  
+  # Set the matrix inverse value
   setInverse <- function(solve) a <- solve 
+  
+  # Get the matrix inverse value
   getInverse <- function() matr
+  
   list(set = set, get = get, 
        setInverse = setInverse, 
        getInverse = getInverse)
